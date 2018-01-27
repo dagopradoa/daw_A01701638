6ee3c5653de07d64f818b41a1d9e2ff5af3e9568
dagopradoa <A01701638@itesm.mx>
2018-01-26 22:18:29 -0600

    subir avance lab5

--- Laboratorio3/lab_five_js.js
+++ Laboratorio3/lab_five_js.js
@@ -24,6 +24,51 @@ function validar(){
     
 }
 
-//plugin bootstrap minus and plus
-$('.btn-number').click(function(e){
-    e.preventDefault();
+//minus and plus
+//salsa roja
+var count_sv = 1;
+var count_salsaverde= document.getElementById("salsa_verde");
+    
+function add_salsaverde(){
+    count_sv++;
+    count_salsaverde.value = count_sr;
+}
+
+function remove_salsaroja(){
+  if (count > 1) {
+    count_sv--;
+    count_salsaverde.value = count_sr;
+  }  
+}
+
+//salsa de cacahuate
+var count_sc = 1;
+var count_salsacacahuate = document.getElementById("salsa_cacahuate");
+    
+function add_salsacacahuate(){
+    count_sc++;
+    count_salsacacahuate.value = count_sc;
+}
+
+function remove_salsacacahuate(){
+  if (count > 1) {
+    count_sc--;
+    count_salsacacahuate.value = count_sc;
+  }  
+}
+
+//guacamole
+var count_sg = 1;
+var count_guacamole = document.getElementById("salsa_guacamole");
+    
+function add_guacamole(){
+    count_sg++;
+    count_guacamole.value = count_sg;
+}
+
+function remove_guacamole(){
+  if (count > 1) {
+    count_sg--;
+    count_guacamole.value = count_sg;
+  }  
+}
