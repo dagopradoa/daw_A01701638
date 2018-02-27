ddc051075a96def6a1a9ef9692ed5882b3fd7b9c
dagopradoa <A01701638@itesm.mx>
2018-02-26 21:37:56 -0600

    finish lab11

--- LaboratoriosPHP/labeleven.php
+++ LaboratoriosPHP/labeleven.php
@@ -168,27 +168,28 @@ function test_input($data) {
 <div class="container container-fluid">
     <div class="jumbotron jumbotron-fluid bg-dark">
         <section>
-            <h2 style="color:aliceblue">Questions</h2>
-            <h3 class="pregunta" style="color:aliceblue">¿Qué es entonces una base de datos - DB?</h3>
-            <p style="color:aliceblue"> [1] Es una herramienta para recopilar y organizar información. Y de acuerdo a lo visto en clase es una coleccion de archivos interrelacionados logicamente.</p>
-            <h3 class="pregunta" style="color:aliceblue">¿En qué casos es conveniente usar bases de datos?</h3>
-            <p style="color:aliceblue">[1] En su mayoría, las bases de datos comienzan como una lista en una hoja de cálculo. Mientras esta lista va cereciendo comienza a hacer incosistencias en los datos. Cada vez es más difícil realizar busquedas o extracción de información; una vez que estos problemas aparecen es una gran idea crear una base de datos administada por DBMS.</p>
-            <h3 class="pregunta" style="color:aliceblue">¿Qué es un sistema de gestión de base de datos - DBMS?</h3>
-            <p style="color:aliceblue">Tener una noción de la mediad de cada uno, pues uno esta en pixeles y el otro en puntos</p>
-            <h3 class="pregunta" style="color:aliceblue">Enlista y explica las funciones/responsabilidades que tienen los DBMS.</h3>
-            <p style="color:aliceblue">[2] Abstracción de datos: ahorra a los usuarios detalles acerca del almacenamiento<br>
-            Independencia: se pueden modificar las bases de datos sin cambiar la aplicacion<br>
-            Consistencia: los datos que ya estan no se repetirán<br>
-            Seguridad: se deben asegurar de que los datos esten protegidos mediante un complejo sistema de permisos<br>
-            Integridad: garantizar la validez de los datos almacenados<br>
-            Respaldo: los DBMS deben realizar copias de respaldo<br>
-            Transacciones: programa que se ejecuta completo o no lo hace<br>
-            Tiempo de respuesta: minimizar el tiempo de una base de datos.</p>
-            <h3 class="pregunta" style="color:aliceblue">Ejemplifica y justifica en que proyecto de los que has realizado hubiera sido conveniente utilizar una base de datos.</h3>
-            <p style="color:aliceblue">En el proyecto de POO, para manejar los objetos en una bases de datos sin que se repitan y jalar la misma imagen de la misma localidad sin necesidad de duplicar las imagenes.</p>
-            <h2 style="color:aliceblue">REFERENCIAS</h2>
-            <p style="color:aliceblue">1.- https://support.office.com/es-es/article/conceptos-b%C3%A1sicos-sobre-bases-de-datos-a849ac16-07c7-4a31-9948-3c8c94a7c204<br>
-            2.- https://aiu.edu/cursos/base%20de%20datos/pdf%20leccion%201/lecci%C3%B3n%201.pdf
+            <h2 >Questions</h2>
+            <h3 class="pregunta">¿Por qué es una buena práctica separar el controlador de la vista?.</h3>
+            <p> Para dividir la complejidad del código y reutilizar el mismo</p>
+            <h3 class="pregunta">Aparte de los arreglos $_POST y $_GET, ¿qué otros arreglos están predefinidos en php y cuál es su función?</h3>
+            <p> [1] $_SERVER, contiene informacion de rutas, cabeceras,etc.<br>
+            $_COOKIE, Asocia variable pasadas mediante uso de cookies<br>
+            $_SESSION, Contien variables de sesion<br>
+            $_REQUEST, Contiene GET,POST y COOKIE<br>
+            $_FILES, asociativo de elementos subidos al script</p>
+            <h3 class="pregunta">Explora las funciones de php, y describe 2 que no hayas visto en otro lenguaje y que llamen tu atención.</h3>
+            <p>[2] mysql_connect, que abre una extension al servidor SQL<br>
+            [3] phpinfo, que despliega información acerca del php que se está utilizando</p>
+            <h3 class="pregunta">¿Qué es XSS y cómo se puede prevenir?</h3>
+            <p>[4] Es un tipo de inyección, en el que scripts del tipo maligno se insertan en sitios web. Los atacantes utilizan aplicaciones que envia código malicioso.<br>
+            [5] Se puede prevenir mediante la codificación de los datos con las funciones 'htmlentities' y 'htmlspecialchars'
+            </p>
+            <h2>REFERENCIAS</h2>
+            <p>1.- http://php.net/manual/es/language.variables.superglobals.php<br>
+            2.- http://php.net/manual/es/function.mysql-connect.php<br>
+            3.- http://php.net/manual/es/function.phpinfo.php<br>
+            4.- https://www.owasp.org/index.php/Cross-site_Scripting_(XSS)<br>
+            5.- https://medium.com/@jpmorris/how-to-prevent-xss-attacks-by-escaping-output-in-php-942204bf184
             </p>
         </section>
     </div>
