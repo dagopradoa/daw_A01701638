3393b7cfb097e7189e2d88a87055c584d4b7bef8
dagopradoa <A01701638@itesm.mx>
2018-03-02 21:25:49 -0600

    add some changes lab14

--- LaboratoriosPHP/lab14/session.php
+++ LaboratoriosPHP/lab14/session.php
@@ -1,24 +1,24 @@
 <?php
     session_start();
     require_once("dbms_14.php");
-    
+
     if(isset($_SESSION["usuario"]) ) {
         include("partials/_header.html");
+        include("partials/_bodynav.html");
         $user = $_SESSION["usuario"];
-        include("partials/_bodynav.html")
         include("partials/_preview.html");
-        include("partials/_footer.html"); 
+        include("partials/_footer.html");
     }else if (login($_POST["usuario"], $_POST["password"]) ) {
         unset($_SESSION["error"]);
         $_SESSION["usuario"] = $_POST["usuario"];
         $user = $_SESSION["usuario"];
         include("partials/_header.html");
-        include("partials/_bodynav.html")
+        include("partials/_bodynav.html");
         include("partials/_preview.html");
-        include("partials/_footer.html"); 
+        include("partials/_footer.html");
     } else if($_POST["usuario"]!="dago" || $_POST["password"]!="1701638") {
         $_SESSION["error"] = "Credenciales inválidas";
         header("location: lab14.php");
     }
-    
+
 ?>
